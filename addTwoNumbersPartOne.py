8f459ced013a5ab29e616782dee09e52a0fb3874
Vivekananthan Dhamodharan <88820633+dvivekananthanece@users.noreply.github.com>
2023-05-18 08:53:11 +0530

    Add files via upload

---
+++ addTwoNumbersPartOne.py
@@ -0,0 +1,56 @@
+from LeetCode import linkedList as ll
+from typing import Optional
+
+linkedList = ll.head()
+linkedList.head = ll.Node(2)
+# Node2 = ll.Node(2)
+Node3 = ll.Node(4)
+Node4 = ll.Node(3)
+linkedList.head.next = Node3
+# Node2.next = Node3
+Node3.next = Node4
+
+linkedList2 = ll.head()
+linkedList2.head = ll.Node(5)
+Node9 = ll.Node(6)
+Node8 = ll.Node(4)
+linkedList2.head.next = Node9
+Node9.next = Node8
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.next = next
+        self.val = val
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        list1 = []
+        list2 = []
+        newList = None
+        while l1:
+            list1.append(l1.data)
+            l1 = l1.next
+        while l2:
+            list2.append(l2.data)
+            l2 = l2.next
+        l1.reverse()
+        l2.reverse()
+        carry = 0
+        result = None
+        while list1 or list2 or carry:
+            obj1 = list1.pop() if list1 else 0
+            obj2 = list2.pop() if list2 else 0
+            carry, val = divmod(obj1 + obj2 + carry, 10)
+
+            if result is None:
+                result = newList = ListNode(val)
+            else:
+                newList.next = ListNode(val)
+                newList = newList.next
+        return result
+
+
+solution = Solution()
+solution.addTwoNumbers(linkedList.head, linkedList2.head)
